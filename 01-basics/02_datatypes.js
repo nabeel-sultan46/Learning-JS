5e6cea886f4425992cdf401d7984bb64f827e626
nabeel-sultan46 <157129769+nabeel-sultan46@users.noreply.github.com>
2024-05-30 13:43:52 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all the code as newer version.
+
+// alert(3+3) // it will not work in it bcs we are using nodejs not browser which has script to use it.it has diff method in nodejs.
+
+console.log(3+3) // always focus on readablity.It should be high.
+
+console.log("Nabeel");
+
+
+// for datatypes
+
+let name="nabeel"
+let age = 18
+let isloggedIn= false
+let state;
+
+// number => 2 to power 53
+// bigint => for more big values than a number can store
+// boolean=> true / false
+// null => standalone value. for exmple we use null for tempreture if there is error in the sever data fetching.
+// undefined =>
+// symbol => unique. Mostly used in the react and building of figma components in UI/UX/
+
+// object
+
+console.log(typeof "nabeel");
+console.log(typeof age);
+console.log(typeof isloggedIn);
+console.log(typeof null); // type shown is object
+console.log(typeof undefined); // type is shown undefined 
